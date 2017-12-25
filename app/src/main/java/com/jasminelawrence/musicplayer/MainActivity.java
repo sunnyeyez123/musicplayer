cd93905d1f9f3e2bb0ddc916069b65ccaafb4100
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2017-12-24 19:14:32 -0800

    first commit

---
+++ app/src/main/java/com/jasminelawrence/musicplayer/MainActivity.java
@@ -0,0 +1,27 @@
+package com.jasminelawrence.musicplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void playSong(View view){
+
+
+
+    }
+
+
+    public void pauseSong(View view){
+
+
+
+    }
+}
