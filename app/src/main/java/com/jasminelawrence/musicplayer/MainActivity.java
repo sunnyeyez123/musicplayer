30ae2935db14408e4e6060b031c1ee2bd22665ac
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2017-12-24 20:22:59 -0800

    Move play and pause from onclick in xml to onclicklisteners

--- app/src/main/java/com/jasminelawrence/musicplayer/MainActivity.java
+++ app/src/main/java/com/jasminelawrence/musicplayer/MainActivity.java
@@ -4,38 +4,62 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mMediaPlayer;
-    float volume = (float)1.0;
+    private Button playButton,pauseButton, resetButton, volUpButton, volDownButton;
+    private float volume = (float)1.0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mMediaPlayer = MediaPlayer.create(this, R.raw.baby_music_box_daniel_simion);
+
+
+
+        playButton = (Button) findViewById(R.id.play_button);
+          pauseButton = (Button) findViewById(R.id.pause_button);
+
+        pauseButton.setEnabled(false);
+
+         resetButton = (Button)findViewById(R.id.reset_button);
+          volUpButton = (Button)findViewById(R.id.volup_button);
+          volDownButton = (Button)findViewById(R.id.voldown_button);
+
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mMediaPlayer.start();
+                Toast.makeText(getApplicationContext(), "Playing song",Toast.LENGTH_SHORT).show();
+                pauseButton.setEnabled(true);
+                playButton.setEnabled(false);
+
+            }
+        });
+
+
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mMediaPlayer.pause();
+                Toast.makeText(getApplicationContext(), "Pausing song",Toast.LENGTH_SHORT).show();
+                pauseButton.setEnabled(false);
+                playButton.setEnabled(true);
+
+            }
+        });
+
     }
 
-    public void playSong(View view){
 
-        mMediaPlayer.start();
-        Toast.makeText(getApplicationContext(), "Playing song",Toast.LENGTH_SHORT).show();
-
-
-
-
-    }
-
-
-    public void pauseSong(View view){
-        mMediaPlayer.pause();
-
-        Toast.makeText(getApplicationContext(), "Pausing song",Toast.LENGTH_SHORT).show();
-
-
-    }
 
     public void resetSong(View view){
         mMediaPlayer.seekTo(0);
